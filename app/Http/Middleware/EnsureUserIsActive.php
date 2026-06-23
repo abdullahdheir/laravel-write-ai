f66af7a376dbf4371c512e657a0fd2fc43243f71
Abdullah Dheir <tareqm577@gmail.com>
2026-06-23 18:40:18 +0300

    feat: implement ensure user activation middleware

---
+++ app/Http/Middleware/EnsureUserIsActive.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsActive
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->status !== 'active') {
+            Auth::logout();
+            return redirect()->route('login')->withErrors(['Your account is not active. Please contact support.']);
+        }
+        
+        return $next($request);
+    }
+}
